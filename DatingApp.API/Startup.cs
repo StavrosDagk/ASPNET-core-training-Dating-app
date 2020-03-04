4e45096ecd2b29d5d9a2295f796cb200beaa2498
DAGKALAKIS Stavros <stavros.dagkalakis@akka.eu>
2020-03-04 16:18:13 +0100

    cors added, angular cli added, values displayed.

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -29,6 +29,8 @@ namespace DatingApp.API
         {
             services.AddDbContext<DataContext>( x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,12 +44,13 @@ namespace DatingApp.API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                // endpoints.MapFallbackToAreaController("Index","Fallback");
             });
         }
     }
